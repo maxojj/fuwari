9d2102bab74886d4f41182189f7c19ca2ca46a9e
maxojj <31129637+maxojj@users.noreply.github.com>
2026-08-10 19:24:25 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,9 +8,9 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
-	subtitle: "Demo Site",
-	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	title: "西灵沉船",
+	subtitle: "西灵号 欢迎你",
+	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
@@ -84,12 +84,6 @@ export const navBarConfig: NavBarConfig = {
 		// About
 		LinkPreset.About,
 
-		// GitHub
-		{
-			name: "GitHub",
-			url: "https://github.com/maxojj",
-			external: true,
-		},
 	],
 };
 
